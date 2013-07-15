87207e491cb088314fc2139b6f9e9c71e8003aed
niels <opensource21@gmail.com>
2013-07-15 10:24:19 +0200

    BS JS- erweitert. Hier muss man noch mal refactorn.

--- src/main/webapp/resources/js/thymelaefbs.js
+++ src/main/webapp/resources/js/thymelaefbs.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     bsfield();
+    bsname();
+    bsfieldConstructor('field');
 });
 
 function bsfield() {
@@ -19,4 +21,42 @@ function bsfield() {
        input.replaceWith(controlgroup);
        controls.append(input);
    });
+}
+
+function bsname() {
+       $('*[bs\\:name]').each(function( index ) {
+           var fieldname = $(this).attr('bs:name');
+           var input = $(this)
+           var controlgroup=$('<div></div>');
+           controlgroup.addClass("control-group");
+           var label=$('<label></label>');
+           label.addClass("control-label");
+           label.append(fieldname);
+           var controls = $('<div></div>');
+           controls.addClass("controls");
+
+           controlgroup.append(label);
+           controlgroup.append(controls);
+           input.replaceWith(controlgroup);
+           controls.append(input);
+       });
+    }
+
+function bsfieldConstructor(name) {
+    $('*[bs\\:'+name).each(function( index ) {
+        var fieldname = $(this).attr('bs:'+name);
+        var input = $(this)
+        var controlgroup=$('<div></div>');
+        controlgroup.addClass("control-group");
+        var label=$('<label></label>');
+        label.addClass("control-label");
+        label.append(fieldname);
+        var controls = $('<div></div>');
+        controls.addClass("controls");
+
+        controlgroup.append(label);
+        controlgroup.append(controls);
+        input.replaceWith(controlgroup);
+        controls.append(input);
+    });
 }
