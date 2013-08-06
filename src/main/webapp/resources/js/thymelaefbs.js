b59f698f00d09becfb199401570143d00417af64
niels <opensource21@gmail.com>
2013-08-06 09:08:40 +0200

    js-gefixt.

--- src/main/webapp/resources/js/thymelaefbs.js
+++ src/main/webapp/resources/js/thymelaefbs.js
@@ -1,49 +1,11 @@
 $(document).ready(function(){
-    bsfield();
-    bsname();
     bsfieldConstructor('field');
+    bsfieldConstructor('name');
 });
 
-function bsfield() {
-   $('*[bs\\:field]').each(function( index ) {
-       var fieldname = $(this).attr('bs:field');
-       var input = $(this)
-       var controlgroup=$('<div></div>');
-       controlgroup.addClass("control-group");
-       var label=$('<label></label>');
-       label.addClass("control-label");
-       label.append(fieldname);
-       var controls = $('<div></div>');
-       controls.addClass("controls");
-
-       controlgroup.append(label);
-       controlgroup.append(controls);
-       input.replaceWith(controlgroup);
-       controls.append(input);
-   });
-}
-
-function bsname() {
-       $('*[bs\\:name]').each(function( index ) {
-           var fieldname = $(this).attr('bs:name');
-           var input = $(this)
-           var controlgroup=$('<div></div>');
-           controlgroup.addClass("control-group");
-           var label=$('<label></label>');
-           label.addClass("control-label");
-           label.append(fieldname);
-           var controls = $('<div></div>');
-           controls.addClass("controls");
-
-           controlgroup.append(label);
-           controlgroup.append(controls);
-           input.replaceWith(controlgroup);
-           controls.append(input);
-       });
-    }
 
 function bsfieldConstructor(name) {
-    $('*[bs\\:'+name).each(function( index ) {
+    $('*[bs\\:'+name+']').each(function( index ) {
         var fieldname = $(this).attr('bs:'+name);
         var input = $(this)
         var controlgroup=$('<div></div>');
