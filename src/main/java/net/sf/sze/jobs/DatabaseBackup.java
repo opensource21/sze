e0beed8ab9377b4baa3adc2aa40f0b21223409a6
niels <opensource21@gmail.com>
2014-01-28 19:54:11 +0100

    Datenbank-Backup eingebaut.

---
+++ src/main/java/net/sf/sze/jobs/DatabaseBackup.java
@@ -0,0 +1,66 @@
+//DatabaseBackup.java
+//
+// Licensed under the AGPL - http://www.gnu.org/licenses/agpl-3.0.txt
+// (c) SZE-Development Team
+package net.sf.sze.jobs;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Job der sicherstellt, dass die Datenbank regelmäßig gesichert wird.
+ *
+ */
+@Service
+@PropertySource("classpath:/app.properties")
+public class DatabaseBackup {
+
+    @Resource
+    private DataSource dataSource;
+
+    @Resource
+    private Environment env;
+
+    /**
+     * The Logger for the controller.
+     */
+    private static final Logger LOG = LoggerFactory
+            .getLogger(DatabaseBackup.class);
+
+
+    /**
+     * Sicherung einer H2-Datenbank.
+     */
+    @Scheduled(cron = "0 0 1 * * *")
+    public void backupH2Database() {
+        if (env.getProperty("db.driver").equals("org.h2.Driver")) {
+            final String backupDir = env.getProperty("backupDir", "databases/backup");
+            try {
+                 final DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                 final File backupFile = new File(backupDir, "sze_"
+                         + dateFormat.format(new Date()) + ".zip");
+                 LOG.info("Backup to {}", backupFile.getAbsolutePath());
+                 dataSource.getConnection().createStatement().execute(
+                         "BACKUP TO '" + backupFile.getAbsolutePath() + "'");
+            } catch (SQLException sqlE) {
+                LOG.error("Couldn't create Database-Backup!", sqlE);
+
+            }
+        }
+
+    }
+}
