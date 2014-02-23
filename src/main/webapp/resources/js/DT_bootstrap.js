a14c6126c9c4a06bc2ed7cb36df7395d495ef297
niels <opensource21@gmail.com>
2014-02-23 15:59:31 +0100

    Die letzte Spalte ist nicht durchsuchbar, bei datatable_with_action.

--- src/main/webapp/resources/js/DT_bootstrap.js
+++ src/main/webapp/resources/js/DT_bootstrap.js
@@ -149,12 +149,18 @@ if ( $.fn.DataTable.TableTools ) {
 
 /* Table initialisation */
 $(document).ready(function() {
-    $('.datatable').dataTable( {
+    $('.datatable_with_action').dataTable( {
         "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
         "sPaginationType": "bootstrap",
         "oLanguage": {
             "sLengthMenu": "_MENU_ records per page"
         },
-        "aaSorting":[]
+        "aaSorting":[],
+        "aoColumnDefs": [
+              {
+                 "bSearchable": false,
+                 "aTargets": [ -1 ]
+              }
+            ]
     } );
 } );
