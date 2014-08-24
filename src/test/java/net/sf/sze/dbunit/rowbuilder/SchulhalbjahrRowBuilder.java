cef1df94292a92260edec7be6aa269fd1820b3a9
niels <opensource21@gmail.com>
2014-08-24 22:02:31 +0200

    RowBuilder für den Tets aktualisiert.

--- src/test/java/net/sf/sze/dbunit/rowbuilder/SchulhalbjahrRowBuilder.java
+++ src/test/java/net/sf/sze/dbunit/rowbuilder/SchulhalbjahrRowBuilder.java
@@ -2,20 +2,23 @@ package net.sf.sze.dbunit.rowbuilder;
 
 import org.dbunit.dataset.builder.BasicDataRowBuilder;
 import org.dbunit.validator.IValidator;
+import java.sql.Date;
 
 public class SchulhalbjahrRowBuilder extends BasicDataRowBuilder {
 
     public static final String TABLE_NAME = "SCHULHALBJAHR";
 
+    public static final String C_AUSGABE_DATUM = "AUSGABE_DATUM";
     public static final String C_HALBJAHR = "HALBJAHR";
     public static final String C_ID = "ID";
     public static final String C_JAHR = "JAHR";
+    public static final String C_NACHTEILS_AUSGLEICHS_DATUM = "NACHTEILS_AUSGLEICHS_DATUM";
     public static final String C_SELECTABLE = "SELECTABLE";
     public static final String C_VERSION = "VERSION";
 
     public static final String[] PRIMARY_KEY = {C_ID};
 
-    public static final String[] ALL_COLUMNS = {C_HALBJAHR, C_ID, C_JAHR, C_SELECTABLE, C_VERSION};
+    public static final String[] ALL_COLUMNS = {C_AUSGABE_DATUM, C_HALBJAHR, C_ID, C_JAHR, C_NACHTEILS_AUSGLEICHS_DATUM, C_SELECTABLE, C_VERSION};
 
     public SchulhalbjahrRowBuilder(String... identifierColumns) {
         super(TABLE_NAME, identifierColumns);
@@ -25,6 +28,16 @@ public class SchulhalbjahrRowBuilder extends BasicDataRowBuilder {
         addDefaultValue(C_ID, new Long("0"));
         addDefaultValue(C_SELECTABLE, Boolean.FALSE);
         addDefaultValue(C_HALBJAHR, new Integer("0"));
+    }
+
+    public final SchulhalbjahrRowBuilder AusgabeDatum (Date value) {
+        with(C_AUSGABE_DATUM, value);
+        return this;
+    }
+
+    public final SchulhalbjahrRowBuilder AusgabeDatum (IValidator<?> value) {
+        with(C_AUSGABE_DATUM, value);
+        return this;
     }
 
     public final SchulhalbjahrRowBuilder Halbjahr (Integer value) {
@@ -54,6 +67,16 @@ public class SchulhalbjahrRowBuilder extends BasicDataRowBuilder {
 
     public final SchulhalbjahrRowBuilder Jahr (IValidator<?> value) {
         with(C_JAHR, value);
+        return this;
+    }
+
+    public final SchulhalbjahrRowBuilder NachteilsAusgleichsDatum (Date value) {
+        with(C_NACHTEILS_AUSGLEICHS_DATUM, value);
+        return this;
+    }
+
+    public final SchulhalbjahrRowBuilder NachteilsAusgleichsDatum (IValidator<?> value) {
+        with(C_NACHTEILS_AUSGLEICHS_DATUM, value);
         return this;
     }
 
