f6a3588bc60f9d42400feb86f9e8900075f7f951
niels <opensource21@gmail.com>
2015-03-14 22:56:05 +0100

    Thymeleaf zum Laufen gebracht.

--- src/main/java/net/sf/sze/config/ThymeleafConfig.java
+++ src/main/java/net/sf/sze/config/ThymeleafConfig.java
@@ -1,14 +1,7 @@
 package net.sf.sze.config;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 import de.ppi.fuwesta.thymeleaf.bootstrap2.Bootstrap2Dialect;
@@ -22,63 +15,30 @@ import de.ppi.fuwesta.thymeleaf.mail.MailToDialect;
 public class ThymeleafConfig {
 
     /**
-     * Die POM-Versions-Nr.
-     */
-    @Value("${project.version}")
-    private String buildNr;
-
-
-    /**
-     * Encoding of HTTP-Response and of the templates.
-     */
-    private String characterEncoding = "UTF-8";
-
-    /**
-     * Configures the Thymeleaf view resolver.
-     *
-     * @return the view resolver.
+     * Register dialect for easy bootstrap forms.
+     * @return the dialect.
      */
     @Bean
-    public ThymeleafViewResolver configureInternalThymeLeafResourceViewResolver() {
-        final ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        resolver.setOrder(2);
-        resolver.setContentType(MediaType.TEXT_HTML_VALUE);
-        resolver.setCharacterEncoding(characterEncoding);
-        return resolver;
+    public Bootstrap2Dialect getBootstrap2Dialect() {
+        return new Bootstrap2Dialect();
     }
 
     /**
-     * Thymeleaf template engine.
-     *
-     * @return Thymeleaf template engine.
+     * Register dialect for mail-links.
+     * @return the dialect.
      */
-    @Bean()
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine ste = new SpringTemplateEngine();
-        ste.setTemplateResolver(templateResolver());
-        ste.addDialect(new LayoutDialect());
-        ste.addDialect(new Bootstrap2Dialect());
-        ste.addDialect(new MailToDialect());
-        ste.addDialect(new ShiroDialect());
-        return ste;
+    @Bean
+    public MailToDialect getMailToDialect() {
+        return new MailToDialect();
     }
 
     /**
-     * Thymeleaf template resolver.
-     *
-     * @return Thymeleaf template resolver.
+     * Register dialect for shiro security.
+     * @return the dialect.
      */
-    @Bean()
-    public ServletContextTemplateResolver templateResolver() {
-        final ServletContextTemplateResolver resolver =
-                new ServletContextTemplateResolver();
-        resolver.setPrefix("/WEB-INF/templates/");
-        resolver.setSuffix(".html");
-        resolver.setTemplateMode("HTML5");
-        resolver.setCacheable(!buildNr.endsWith("-SNAPSHOT"));
-        resolver.setCharacterEncoding("UTF-8");
-        return resolver;
+    @Bean
+    public ShiroDialect getShiroDialect() {
+        return new ShiroDialect();
     }
 
 }
