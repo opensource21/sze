f2dae5d6a28127c20094da4113f7d414c9ab481a
niels <opensource21@gmail.com>
2015-03-15 18:20:08 +0100

    TrackingMode auf Cookie gesetzt.

--- src/main/java/net/sf/sze/ServletInitializer.java
+++ src/main/java/net/sf/sze/ServletInitializer.java
@@ -1,5 +1,12 @@
 package net.sf.sze;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionTrackingMode;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
@@ -13,5 +20,20 @@ public class ServletInitializer extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SzeServer.class);
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onStartup(ServletContext servletContext)
+            throws ServletException {
+        servletContext.getSessionCookieConfig().setHttpOnly(true);
+        final Set<SessionTrackingMode> sessionTrackingModes = new HashSet<>();
+        sessionTrackingModes.add(SessionTrackingMode.COOKIE);
+        servletContext.setSessionTrackingModes(sessionTrackingModes);
+        super.onStartup(servletContext);
+    }
+
+
 
 }
