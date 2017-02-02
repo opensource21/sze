2c587d9eb30043a4f2cb256f3c98bbeb67fb7d87
niels <opensource21@gmail.com>
2017-02-02 11:40:16 +0100

    Den Default bei ID-Generator wieder hergestellt. Jetzt klappt es mit Hibernate 5 und den IDs :-)

--- src/main/java/net/sf/sze/model/base/RevisionLog.java
+++ src/main/java/net/sf/sze/model/base/RevisionLog.java
@@ -11,7 +11,6 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
@@ -40,7 +39,7 @@ public class RevisionLog {
      * The identifier of the entity.
      */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue()
     @Column(unique = true, nullable = false)
     private Long id;
 
