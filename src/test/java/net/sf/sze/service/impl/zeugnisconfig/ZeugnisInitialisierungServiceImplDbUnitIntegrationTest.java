3c5bce1e252ce70dff65e937de74de221fe887c0
niels <opensource21@gmail.com>
2017-02-07 08:41:55 +0100

    Da der ZeugnisInitialierungsService auf dem Zeugnis arbeitet, sollte er auch im Package zeugnis liegen.

--- src/test/java/net/sf/sze/service/impl/zeugnisconfig/ZeugnisInitialisierungServiceImplDbUnitIntegrationTest.java
+++ src/test/java/net/sf/sze/service/impl/zeugnisconfig/ZeugnisInitialisierungServiceImplDbUnitIntegrationTest.java
@@ -17,7 +17,8 @@ import net.sf.sze.dbunit.dataset.InitZeugnis;
 import net.sf.sze.dbunit.dataset.UpdateZeugnis;
 import net.sf.sze.model.zeugnis.Zeugnis;
 import net.sf.sze.model.zeugnis.ZeugnisFormular;
-import net.sf.sze.service.api.zeugnisconfig.ZeugnisInitialierungsService;
+import net.sf.sze.service.api.zeugnis.ZeugnisInitialierungsService;
+import net.sf.sze.service.impl.zeugnis.ZeugnisInitialisierungServiceImpl;
 import net.sf.sze.util.ResultContainer;
 
 import org.dbunit.dataset.IDataSet;
@@ -49,7 +50,7 @@ public class ZeugnisInitialisierungServiceImplDbUnitIntegrationTest
 
     /**
      * Test method for
-     * {@link net.sf.sze.service.impl.zeugnisconfig.ZeugnisInitialisierungServiceImpl#initZeugnis(net.sf.sze.model.zeugnis.ZeugnisFormular)}.
+     * {@link net.sf.sze.service.impl.zeugnis.ZeugnisInitialisierungServiceImpl#initZeugnis(net.sf.sze.model.zeugnis.ZeugnisFormular)}.
      * @throws Exception Fehler
      */
     @Test
@@ -59,7 +60,7 @@ public class ZeugnisInitialisierungServiceImplDbUnitIntegrationTest
 
     /**
      * Test method for
-     * {@link net.sf.sze.service.impl.zeugnisconfig.ZeugnisInitialisierungServiceImpl#initZeugnis(net.sf.sze.model.zeugnis.ZeugnisFormular)}.
+     * {@link net.sf.sze.service.impl.zeugnis.ZeugnisInitialisierungServiceImpl#initZeugnis(net.sf.sze.model.zeugnis.ZeugnisFormular)}.
      * @throws Exception Fehler
      */
     @Test
@@ -103,7 +104,7 @@ public class ZeugnisInitialisierungServiceImplDbUnitIntegrationTest
 
     /**
      * Test method for
-     * {@link net.sf.sze.service.impl.zeugnisconfig.ZeugnisInitialisierungServiceImpl#initZeugnis(net.sf.sze.model.zeugnis.ZeugnisFormular)}.
+     * {@link net.sf.sze.service.impl.zeugnis.ZeugnisInitialisierungServiceImpl#initZeugnis(net.sf.sze.model.zeugnis.ZeugnisFormular)}.
      * @throws Exception Fehler
      */
     @Test
